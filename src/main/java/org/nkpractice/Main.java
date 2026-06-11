89b3c2864862c57a7f71e87c7188390676d24170
nk10008 <nkgupta3541@gmail.com>
2026-06-11 17:39:28 +0530

    second version

--- src/main/java/org/nkpractice/Main.java
+++ src/main/java/org/nkpractice/Main.java
@@ -1,5 +1,7 @@
 package org.nkpractice;
 
+import java.util.function.Predicate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -12,6 +14,9 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+            System.out.println("i * i = " + (i * i));
+            Predicate<Integer> isEven = n -> n % 2 == 0;
+            System.out.println("Is i even? " + isEven.test(i));
         }
     }
 }
